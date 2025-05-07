3f54ad7e57c955ee99a10cd4319dbaa494110f13
im-AnirudhBetrabet <study.datascience.anirudh@gmail.com>
2025-05-07 09:31:55 +0530

    Remove react router

--- src/index.js
+++ src/index.js
@@ -2,30 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
 
 const root = document.getElementById('root');
 
-let router = createBrowserRouter([
-  {
-    path: "/",
-    Component: App 
-  }
-],
-  {
-    basename: "/data-portfolio"
-  });
-
 ReactDOM.createRoot(root).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

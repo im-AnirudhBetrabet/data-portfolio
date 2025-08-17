8449518a4b0c619a1cf43f1d375d732f9d32ecab
im-AnirudhBetrabet <study.datascience.anirudh@gmail.com>
2025-08-17 18:38:58 +0530

    Add projects

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './projects.css';
-
+import projectData from '../data/Projects';
+import ProjectCard from '../projectcard/ProjectCard';
 function Projects() {
   return (
     <React.Fragment>
         <section className="section projects" id="projects" data-aos="fade-left">
             <h2 className="section-title">Projects</h2>
-            <div className="project-placeholder">
-            Project showcase will appear here once completed. Stay tuned!
+            <div className="project-grid">
+              {
+                projectData.map((project, index) => {
+                  return <ProjectCard projectDetails={project} key={index} anim={ index % 2 == 0 ? "fade-right" : "fade-left"}/>
+                })
+              }
             </div>
         </section>
     </React.Fragment>
